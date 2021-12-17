4d761f88e70d7a39e9ca04e1f987021d46737fb0
carlitos-code23 <carlosrifa654@gmail.com>
2021-12-17 11:48:58 -0600

    muestra

--- src/app/components/shared/global.model.ts
+++ src/app/components/shared/global.model.ts
@@ -1,6 +1,6 @@
 
 export class GlobalModel {
-    public static apiURL: string = 'http://localhost:3000';
+    public static apiURL: string = 'http://25.47.229.56:3000';
   
     public static querysArtists = {
       GetAll: '/artista',
